a86130d71f630435421528b7563ee2de9d2fb6a7
Micah <micah@8thlight.com>
2010-12-14 23:37:28 -0600

    solved #2 and #3

--- spec/euler/euler_test.clj
+++ spec/euler/euler_test.clj
@@ -3,6 +3,23 @@
     [speclj.core]
     [euler.euler]))
 
+(describe "Project Euler solutions"
+
+  (it "solves #1"
+    (should= 23 (euler-1 10))
+    (should= 233168 (euler-1 1000)))
+
+
+  (it "solves #2"
+    (should= 2 (euler-2 5))
+    (should= 10 (euler-2 20))
+    (should= 44 (euler-2 100))
+    (should= 4613732 (euler-2 4000000)))
+
+  (it "solves #3"
+    (should= 29 (euler-3 13195))
+    (should= 6857 (euler-3 600851475143))))
+
 (describe "Euler #1"
 
   (it "knows if a number is a multiple of 3 or 5"
@@ -24,13 +41,56 @@
     (should= [1 2] (naturals-upto 3))
     (should= [1 2 3 4] (naturals-upto 5))
     (should= [1 2 3 4 5 6 7 8 9] (naturals-upto 10)))
-
-  (it "solves #1 sample"
-    (should= 23 (euler-1 10)))
-
-  (it "solves the puzzle"
-    (should= 233168 (euler-1 1000)))
-
   )
+
+(describe "Euler #2"
+
+  (it "calculates fibonacci sequence"
+    (should= 0 (nth (fib) 0))
+    (should= 1 (nth (fib) 1))
+    (should= 1 (nth (fib) 2))
+    (should= 2 (nth (fib) 3))
+    (should= 3 (nth (fib) 4))
+    (should= 5 (nth (fib) 5))
+    (should= 8 (nth (fib) 6))
+    (should= 13 (nth (fib) 7)))
+
+  (it "finds fibs upto"
+    (should= [0 1 1 2] (fibs-upto 2))
+    (should= [0 1 1 2 3] (fibs-upto 4))
+    (should= [0 1 1 2 3 5 8] (fibs-upto 10)))
+  )
+
+(describe "prime factors"
+  (it "factors 1"
+    (should= [] (factors-of 1)))
+
+  (it "factors 2"
+    (should= [2] (factors-of 2)))
+
+  (it "factors 3"
+    (should= [3] (factors-of 3)))
+
+  (it "factors 4"
+    (should= [2 2] (factors-of 4)))
+
+  (it "factors 5"
+    (should= [5] (factors-of 5)))
+
+  (it "factors 6"
+    (should= [2 3] (factors-of 6)))
+
+  (it "factors 7"
+    (should= [7] (factors-of 7)))
+
+  (it "factors 8"
+    (should= [2 2 2] (factors-of 8)))
+
+  (it "factors 9"
+    (should= [3 3] (factors-of 9)))
+
+  (it "factors 2^100"
+    (should= (repeat 100 2) (factors-of (Math/pow 2 100)))))
+
 
 (run-specs)
--- src/euler/euler.clj
+++ src/euler/euler.clj
@@ -13,3 +13,31 @@
 (defn euler-1 [n]
   (apply +
     (filter multiple? (naturals-upto n))))
+
+(defn fib []
+  (map first
+    (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
+
+(defn fibs-upto
+  ([n] (fibs-upto n (fib)))
+  ([n fibs]
+    (lazy-seq
+      (let [next-fib (first fibs)]
+        (if (> next-fib n)
+          nil
+          (cons next-fib (fibs-upto n (rest fibs))))))))
+
+(defn euler-2 [n]
+  (apply +
+    (filter even? (fibs-upto n))))
+
+(defn factors-of [n]
+  (loop [factors [] divisor 2 n n]
+    (if (<= n 1)
+      factors
+      (if (= 0 (mod n divisor))
+        (recur (conj factors divisor) divisor (/ n divisor))
+        (recur factors (inc divisor) n)))))
+
+(defn euler-3 [n]
+  (last (factors-of n)))
