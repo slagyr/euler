312779c2d481b3699a5e37b219d09aee5e0c539c
Micah <micahmartin@gmail.com>
2013-04-03 09:43:30 -0500

    updating deps

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject euler "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[speclj "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dev-dependencies [[speclj "2.5.0"]]
   :test-path "spec/"
   )
