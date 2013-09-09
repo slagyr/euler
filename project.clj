678c9ec237d89e7a503454f4804b86445d525f21
Andrew James Zures <andrew.zures@gmail.com>
2013-09-09 15:56:29 -0500

    updated clojure version, spec is working

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject euler "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :profiles {:dev {:dependencies [[speclj "2.7.5"]]}}
   :plugins[[speclj "2.7.5"]]
