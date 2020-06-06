7d475069f9e8fa17bf71a6e0a45e4bd4e86396ef
Micah <micahmartin@gmail.com>
2020-06-06 09:58:39 -0700

    update generator and removes idea files

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec/"]
+  :aliases {"euler" ["run" "-m" "euler.generator"]}
   )
--- spec/euler/generator.clj
+++ spec/euler/generator.clj
@@ -1,50 +1,51 @@
-(ns leiningen.euler
-  (:import
-    [java.io FileOutputStream]))
+(ns euler.generator
+  (:import (java.io File FileOutputStream)
+           (java.text DecimalFormat))
+  (:require [clojure.string :as str]))
 
 (def spec-template
-"(ns euler.level!L!.problem!NS!-spec
-  (:require
-    [speclj.core :refer :all]
-    [euler.level!L!.problem!NS!]))
+  "(ns euler.level!L!.problem!NS!-spec
+    (:require
+      [speclj.core :refer :all]
+      [euler.level!L!.problem!NS! :refer :all]))
 
-; http://projecteuler.net/index.php?section=problems&id=!N!
+  ; http://projecteuler.net/index.php?section=problems&id=!N!
 
-(describe \"Euler Problem #!N!\"
+  (describe \"Euler Problem #!N!\"
 
-  (it \"Solves #!N!\"
-    (should= -1 (euler-!N! -1)))
+    (it \"Solves #!N!\"
+      (should= -1 (euler-!N! -1)))
+    )
+
+  (run-specs)
+  "
   )
-
-(run-specs)
-"
-)
 
 (def src-template
-"(ns euler.level!L!.problem!NS!)
+  "(ns euler.level!L!.problem!NS!)
 
-(defn euler-!N! [n]
-  )
-"
+  (defn euler-!N! [n]
+    )
+  "
   )
 
 (defn render [src mapping]
   (reduce
-    (fn [src [key value]] (.replace src (format "!%s!" key) (str value)))
+    (fn [src [key value]] (str/replace src (format "!%s!" key) (str value)))
     src
     (seq mapping)))
 
 (defn write-file [path content]
-  (.mkdirs (.getParentFile (java.io.File. path)))
+  (.mkdirs (.getParentFile (File. path)))
   (let [output (FileOutputStream. path)
         bytes (.getBytes content)]
     (println "writing" (count bytes) "bytes to" path)
     (.write output (.getBytes content))
     (.close output)))
 
-(defn euler [n]
+(defn -main [n]
   (let [n (Integer/parseInt n)
-        n-str (.format (java.text.DecimalFormat. "000") n)
+        n-str (.format (DecimalFormat. "000") n)
         level (inc (quot (dec n) 25))
         spec-content (render spec-template {"N" n "NS" n-str "L" level})
         spec-file (format "spec/euler/level%s/problem%s_spec.clj" level n-str)
