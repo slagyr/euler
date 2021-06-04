771e44faa1c895a1f9e78491607463b37675ef62
Heui Park <heuipark@Heuis-MacBook-Pro.local>
2021-06-03 19:31:33 -0700

    fresh start to euler 7

--- spec/euler/level1/problem007_spec.clj
+++ spec/euler/level1/problem007_spec.clj
@@ -5,19 +5,6 @@
 
 ; https://projecteuler.net/problem=7
 
-(describe "Euler Problem #7"
-  (it "Checks if number is prime"
-    (should= false (prime? 6))
-    (should= true (prime? 2))
-    (should= true (prime? 3))
-    (should= false (prime? 192))
-    (should= 29 (prime? 29))
-    (should= true (prime? 1)) ;;1 is not prime...
-    )
-
-  (it "Solves #7"
-    (should= 104743 (euler-7 -1))
-     )
-  )
+(describe "euler 7")
 
 (run-specs)
