1760ae2487c057f6eadda7a249a6984e378d0dfe
Heui Park <heuipark@Heuis-MacBook-Pro.local>
2021-06-05 22:07:42 -0700

    solve euler 7

--- spec/euler/level1/problem007_spec.clj
+++ spec/euler/level1/problem007_spec.clj
@@ -6,14 +6,23 @@
 ; https://projecteuler.net/problem=7
 
 (describe "euler 7"
- (xit (it "gets nth prime"
-       (should= 2 (get-nth-prime 1))
-       (should= 5 (get-nth-prime 3))
-       ))
- (it "checks if n is prime"
-  (should (prime? 2))
-  (should-not (prime? 4))
+
+ (it "check if n is not divisble"
+  (should (non-divisible? 3 2))
+  (should-not (non-divisible? 4 2))
+  (should (non-divisible? 4 3))
   )
+ (it "checks if n is prime"
+       (should (prime? 2))
+       (should-not (prime? 4))
+       )
+ (it "solve euler 7"
+       (should= 2 (euler-7 1))
+       (should= 5 (euler-7 3))
+       (should= 7927 (euler-7 1001))
+       (should= 104743 (euler-7 10001))
+       )
+
  )
 
 (run-specs)
--- src/euler/level1/problem007.clj
+++ src/euler/level1/problem007.clj
@@ -1,12 +1,15 @@
 (ns euler.level1.problem007
  (:require [clojure.math.numeric-tower :as math]))
 
-(defn prime? [n]
- (zero? (count (filter #(true? %) (map (= 0 (mod n %) ) (range 1 (inc n)))))))
+;(defn prime? [n]
+; (zero? (count (filter #(true? %) (map #(= 0 (mod n %) ) (range 1 (inc n)))))))
 
-(defn get-nth-prime [n]
- 2)
+(defn non-divisible? [n div]
+ (not (= 0 (mod n div))))
+
+(defn prime? [n]
+ (= 0 (count (filter #(false? (non-divisible? n %))  (range 2 (inc (int (Math/sqrt n))))))))
 
 (defn euler-7 [n]
-
+ (nth (filter #(prime? %) (drop 2 (range))) (dec n))
  )
